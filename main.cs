991dcfdff4b98b4eb80b6a5ab87807ec6abb6c9c
Imal-Kovinda <83854345+Imal-Kovinda@users.noreply.github.com>
2022-09-19 18:36:55 +0530

    first

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class FindMe
+{
+    public static void Main(string[] args)
+    {
+        int X;
+        int Y = 6;
+        int Z = 5;
+        
+        X = Z + Y*Z - Y;
+        Console.WriteLine (X);
+    }
+}
